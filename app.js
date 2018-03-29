cf13774b3b2700a7c7046717105d5d6f736a93ce
Edward Nimocks <enimocks@gmail.com>
2018-03-29 12:10:37 -0400

    more functions/events added

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
 // VARIABLES
 // =============================================================================================
-var animalsArr = ["wolf", "eagle", "leopard", "mountain goat"];
-
-
-
-
-
-
-
-
-
+var animalsArr = ["wolf", "eagle", "leopard", "mountain goat", "shark", "dolphin", "dog"];
 
 
 // FUNCTIONS
 // =============================================================================================
 function displayAnimalGifs() {
   var animal = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=QOs5UwUNB92cGzHglrKf0ueiYv5Fu4J9&limit=5";
+  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=QOs5UwUNB92cGzHglrKf0ueiYv5Fu4J9&limit=5`;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
 
+    console.log(queryURL);
+    console.log(response.data);
     console.log(response.data[0].images.fixed_height.url);
-    
+
     var results = response.data;
 
-  })
+    for (var i = 0; i < results.length; i++) {
+      // Creating a paragraph tag with the result item's rating
+      var p = $("<p>").text("Rating: " + results[i].rating);
+      // Creating and storing an image tag
+      var animalImage = $("<img>");
+      // Setting the src attribute of the image to a property pulled off the result item
+      animalImage.attr("src", results[i].images.fixed_height.url);
+
+      $('#animals').prepend(p);
+      $('#animals').prepend(animalImage);
+    }
+
+  });
 
 }
 
+function clickToPlay() {
 
-
-
-
+}
 
 function renderButtons() {
 
-  // Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
+  // Deletes the animals prior to adding new animals (this is necessary otherwise you will have repeat buttons)
   $("#animalButtons").empty();
 
   // Loops through the array of animals
   for (var i = 0; i < animalsArr.length; i++) {
 
-    // Then dynamicaly generates buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    // Then dynamicaly generates buttons for each animal in the array
     var a = $("<button>");
-    // Adds a class of movie to our button
+    // Adds a class of animal to our button
     a.addClass("animal");
     // Added a data-attribute
     a.attr("data-name", animalsArr[i]);
@@ -57,6 +60,21 @@ function renderButtons() {
     $("#animalButtons").append(a);
   }
 }
+
+// EVENTS
+// =============================================================================================
+$("#addAnimal").on("click", function(event) {
+  event.preventDefault();
+  // This line of code will grab the input from the textbox
+  var animal = $("#animal-input").val().trim();
+
+  // The movie from the textbox is then added to our array
+  animalsArr.push(animal);
+
+  // Calling renderButtons which handles the processing of our animal array
+  renderButtons();
+
+});
 
 $(document).on("click", ".animal", displayAnimalGifs);
 
